6c6c292756f89d38293b875ebf1a20939b7f5734
PortikCoder <portikHun@gmail.com>
2017-12-27 15:13:13 +0200

    communication is realized between the two Points(Activity-Firebase)

--- app/src/main/java/com/example/dell_5548/eventmusicpestyah_hunyi/NewEventActivity.java
+++ app/src/main/java/com/example/dell_5548/eventmusicpestyah_hunyi/NewEventActivity.java
@@ -11,15 +11,33 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class NewEventActivity extends AppCompatActivity {
 
-    private Button mLoginBT;
+    private static final String M_NODE_EVENT = "Events";
+    private static final String M_EVENT_NAME = "name";
+    private static final String M_EVENT_ID = "ID";
+    private static final String M_EVENT_LOCATION_NAME = "location_name";
+    private static final String M_EVENT_TYPE = "type";
+    private static final String M_EVENT_TIME = "time";
+
+    private Button mCreateBT;
+    private Button mCloseBT;
     private EditText mEmailET;
     private EditText mPasswordET;
 
     private FirebaseAuth mFirebaseAuth;
+    //private StorageReference mStorageRef;
+    //private PrivateStorageRefence mPrivStorageRef;
+    private DatabaseReference mDatabaseRef;
     private ProgressBar progressBar;
 
     @Override
@@ -28,13 +46,15 @@ public class NewEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_newevent);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth.signInWithEmailAndPassword("testhun@email.com","testHun");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
 
         RelativeLayout layout = findViewById(R.id.registerRL);
-        mLoginBT = (Button) findViewById(R.id.closeBT);
+        mCreateBT = (Button) findViewById(R.id.createBT);
         mEmailET = (EditText) findViewById(R.id.emailET);
         mPasswordET = (EditText) findViewById(R.id.passwordET);
 
-        mLoginBT.setOnClickListener(new View.OnClickListener() {
+        mCreateBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("SIGN", "IM IN ON CLICK LISTENER!");
@@ -53,11 +73,14 @@ public class NewEventActivity extends AppCompatActivity {
 
     private void createTheEvent() {
         // Istvan, ive to tell you smth, you have to work in plus, to repair this fnc.
-        Log.i("SIGN", "IM IN REGISTER-USER");
+        Log.i("SIGN", "IM IN CREATE EVENT");
+        // Here u have to get all the datas from the UI
         String emailStr = mEmailET.getText().toString().trim();
-        String passwordStr = mPasswordET.getText().toString().trim();
+        String passwordStr = mPasswordET.getText().toString();
+        String eventName = "Piano opera";
 
         Log.i("SIGN", "Email: " + emailStr + "|pass:" + passwordStr);
+
         if (TextUtils.isEmpty(emailStr)) {
             // the email field is empty
             Toast.makeText(this, "Please fill in the Email field.", Toast.LENGTH_SHORT).show();
@@ -73,6 +96,34 @@ public class NewEventActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);  //To show ProgressBar
 
         // saving datas to the FIREBASE
+        String userID = mFirebaseAuth.getCurrentUser().getUid();
+        // Creating event with a title and an ID
+
+        // Put the local variables in a hashMap
+        Map newEventDatas = new HashMap();
+        newEventDatas.put(M_EVENT_NAME, eventName);
+        newEventDatas.put(M_EVENT_TYPE, "piano");
+        newEventDatas.put(M_EVENT_LOCATION_NAME, "tudor");
+        newEventDatas.put(M_EVENT_TIME, "15:15:000");
+        newEventDatas.put(M_EVENT_ID, "UNIQ_ID");
+        // Send this variable collection to the Server =>
+        //FirebaseDatabase.getInstance().getReference("Events").getReference
+            // if we want that the new event name to be created as a UniqID =>
+                // Object eventID = UUID.randomUUID().toString().replaceAll("-","").toUpperCase();
+                // mDatabaseRef.child(M_NODE_EVENT).setValue(eventID);
+                // mDatabaseRef.child(M_NODE_EVENT).getDatabase().getReference(eventID).setValue(newEventDatas);
+
+            // but if we want that the new event to be created as a name =>
+        //mDatabaseRef.child(M_NODE_EVENT).child(eventName);
+        mDatabaseRef.getDatabase().getReference(M_NODE_EVENT).child(eventName).setValue(newEventDatas);
+        //events.getDatabase().getReference("")
+
+        /*** Instructions:
+         * 1. mDatabaseRef = FirebaseDatabase.getinst.getref event.cid = UUID.randomUUID().toString().replaceAll("-","").toUpperCase();
+         * 2. child event. child userList
+         * 3. firabeseStorage.getinst.getref
+         * 4. kep: storageRef.child foto . child uri . getLastPathFragmeent
+         * */
 
         try {
             Thread.sleep(500);
